4bbd92fef3a4a72c6fcb92f187a4cfa3624c2ab6
Emiliano Firmino <emiliano.firmino@gmail.com>
2014-03-31 06:23:21 -0300

    fixed ooerlang MPI_Benchmark tests
    - removed ooe.erl, reference directly using -pa when run erl
    - fixed incorrect reference to proc in pingping and pingpong
    
    Signed-off-by: Emiliano Firmino <emiliano.firmino@gmail.com>

--- examples/MPI_Benchmark/source_code/pingping/ooerlang/conf.hrl
+++ examples/MPI_Benchmark/source_code/pingping/ooerlang/conf.hrl
@@ -1,4 +1,4 @@
 %-define(OUT_FILE, "erl_out.txt").
 -define(ERR_REPORT(Msg, Reason), io:format("%%%%%%%%%%%%%% ERRO %%%%%%%%%%%%%%\n" ++
-				     "Msg: ~w\n" ++
-				     "Reason: ~w\n\n", [Msg, Reason])).
+				     "Msg: ~s\n" ++
+				     "Reason: ~s\n\n", [Msg, Reason])).
--- examples/MPI_Benchmark/source_code/sendrecv/ooerlang/conf.hrl
+++ examples/MPI_Benchmark/source_code/sendrecv/ooerlang/conf.hrl
@@ -1,5 +1,5 @@
 %-define(OUT_FILE, "erl_out.txt").
 -define(ERR_REPORT(Msg, Reason), io:format("%%%%%%%%%%%%%% ERRO %%%%%%%%%%%%%%\n" ++
-				     "Msg: ~w\n" ++
-				     "Reason: ~w\n\n", [Msg, Reason])).
+				     "Msg: ~s\n" ++
+				     "Reason: ~s\n\n", [Msg, Reason])).
 -define(OUT_PATH, "out_erl_threadring.txt").
