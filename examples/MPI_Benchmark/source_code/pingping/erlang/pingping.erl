f97d44837e55aae1bf15aca1488f78452faa15a5
Emiliano Firmino <emiliano.firmino@gmail.com>
2014-06-02 21:28:33 -0300

    fixed inconvenient comparison
    Signed-off-by: Emiliano Firmino <emiliano.firmino@gmail.com>

--- examples/MPI_Benchmark/source_code/pingping/erlang/pingping.erl
+++ examples/MPI_Benchmark/source_code/pingping/erlang/pingping.erl
@@ -13,22 +13,25 @@ run(DataSize, R) ->
 
 run(DataSize, R, OutFile) ->
 	Data = generate_data(DataSize),
+
 	Self = self(),
 	SpawnStart = time_microseg(),
 	P1 = spawn(fun() -> pingping(Data, Self, R) end),
 	P2 = spawn(fun() -> pingping(Data, Self, R) end),
 	SpawnEnd = time_microseg(),
+
 	TimeStart = time_microseg(),
 	P1 ! {init, self(), P2},
 	P2 ! {init, self(), P1},
 	finalize(P1),
 	finalize(P2),
 	TimeEnd = time_microseg(),
+
 	TotalTime = TimeEnd - TimeStart,
 	SpawnTime = SpawnEnd - SpawnStart,
-	 printResult(Data, R, TotalTime, SpawnTime, OutFile).
+	printResult(Data, R, TotalTime, SpawnTime, OutFile).
 
-pingping(_,Parent, 0) ->
+pingping(_, Parent, 0) ->
 	Parent ! {finish, self()};
 
 pingping(Data, Parent, R) ->
@@ -36,20 +39,19 @@ pingping(Data, Parent, R) ->
 		{init, Parent, Peer} ->
 			Peer ! {self(), Data},
 			pingping(Data, Parent, R-1);
-		{Peer, Data} ->
+		{Peer, _} ->
 			Peer ! {self(), Data},
 			pingping(Data, Parent, R-1)
 	end.
 
-finalize(P1) ->
+finalize(Pid) ->
 	receive
-		{finish, P1} ->
-			ok
+		{finish, Pid} -> ok
 	end.
 
 printResult(Data, R, Time_exec, Time_spawn, OutFile) ->
 	FormatH = "~-9s\t ~-13s\t ~-17s\t ~-11s\t ~-10s~n",
-	Header = ["#bytes", "#repetitions", "exec_time[µsec]", "MBytes/sec", "spawn_time"],
+	Header = ["#bytes", "#repetitions", "exec_time[microsec]", "MBytes/sec", "spawn_time"],
 	io:format(OutFile, FormatH, Header),
 	MBps = bandwidth_calc(Data, Time_exec),
 	FormatD = "~-9w\t ~-13w\t ~-17.2f\t ~-11.6f\t ~-15.2f~n",
@@ -69,4 +71,4 @@ generate_data(Size, Bytes) ->
 
 time_microseg() ->
 	{MS, S, US} = now(),
-	(MS * 1.0e+12) + (S * 1.0e+6) + US.
+	(MS * trunc(1.0e+12)) + (S * trunc(1.0e+6)) + US.
